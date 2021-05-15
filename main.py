be6f20f72e98e308e703bc387540b8c6197df734
rude-rodent <1905894@brunel.ac.uk>
2021-05-15 17:40:44 +0100

    Added the ability to shoot bullets in the direction of the mouse at a constant velocity. Made use of pygame's built in Vector2 and normalize().

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ screenHeight = 720
 playerImage = pygame.image.load("player.png")
 bulletImage = pygame.image.load("bullet.png")
 moveSpeed = 0.3
+playerBulletSpeed = 1
 
 
 class Player:
@@ -52,11 +53,14 @@ class Player:
             self.y += moveSpeed
 
     def fire(self):
+        # Use the mouse and player positions to create a Vector2 between them. Normalise the vector to remove magnitude.
         mouseX, mouseY = pygame.mouse.get_pos()
         direction = pygame.math.Vector2(mouseX - self.x, mouseY - self.y).normalize()
+        # Position of the player.
         position = (self.x, self.y)
+        # Instantiate a bullet; pass it the position and direction values.
         bulletInstance = Bullet(position, direction)
-        bulletList.append(bulletInstance)
+        playerBulletList.append(bulletInstance)
 
     def draw(self):
         # Blit the rotated image to the position of the rotated image's rect.
@@ -67,21 +71,25 @@ class Bullet:
 
     def __init__(self, position, direction):
         self.image = bulletImage
-        self.bulletSpeed = 1
+        self.bulletSpeed = playerBulletSpeed
+        # The x and y values from the position tuple.
         self.x = position[0]
         self.y = position[1]
+        # The direction, a Vector2.
         self.direction = direction
 
     def move(self):
+        # Add the x and y values from the direction Vector2, multiplied by the bullet speed.
         self.x += self.direction.x * self.bulletSpeed
         self.y += self.direction.y * self.bulletSpeed
 
     def draw(self):
+        # Draw the bullet at its updated position.
         screen.blit(self.image, (self.x, self.y))
 
 
 playerInstance = Player()
-bulletList = []
+playerBulletList = []
 background = (200, 30, 30)
 
 running = True
@@ -97,7 +105,7 @@ while running:
     playerInstance.move()
     playerInstance.look()
     playerInstance.draw()
-    for bullet in bulletList:
+    for bullet in playerBulletList:
         bullet.move()
         bullet.draw()
 
